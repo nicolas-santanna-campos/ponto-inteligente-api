baa07d97f94d54114d3ae778e27a5ad6a97118f3
nicolas-santanna-campos <nicolassant.anna@gmail.com>
2018-04-07 21:00:44 -0300

    Criando o serviço de Empresa juntamente com os testes no Junit.

---
+++ src/main/java/com/santannacampos/pontointeligente/api/services/impl/EmpresaServiceImpl.java
@@ -0,0 +1,35 @@
+package com.santannacampos.pontointeligente.api.services.impl;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.santannacampos.pontointeligente.api.entities.Empresa;
+import com.santannacampos.pontointeligente.api.repositories.EmpresaRepository;
+import com.santannacampos.pontointeligente.api.services.EmpresaService;
+
+@Service
+public class EmpresaServiceImpl implements EmpresaService{
+
+	private static final Logger log = LoggerFactory.getLogger(EmpresaServiceImpl.class);
+	
+	@Autowired
+	private EmpresaRepository EmpresaRepository;
+	
+	@Override
+	public Optional<Empresa> buscarPorCnpj(String cnpj) {
+		log.info("Buscando uma empresa para o CNPJ: {}", cnpj);
+		return Optional.ofNullable(EmpresaRepository.findByCnpj(cnpj));
+	}
+
+	@Override
+	public Empresa persistir(Empresa empresa) {
+		log.info("Persistindo empresa: {}", empresa);
+		return this.EmpresaRepository.save(empresa);
+	}
+
+	
+}
